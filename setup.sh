06aadf9c8e94e90e1476af3693a2c959ebd5082a
Nick Batsaras <batsarasnikos@gmail.com>
2017-11-25 23:23:31 +0200

    Unstable: Add mpd config

--- setup.sh
+++ setup.sh
@@ -6,3 +6,5 @@ ln -s ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
 
 ln -s ~/.dotfiles/i3/config ~/.config/i3/config
 ln -s ~/.dotfiles/i3/i3blocks.conf ~/.config/i3/i3blocks.conf
+
+ln -s ~/.dotfiles/mpd/mpd.conf ~/.config/mpd/mpd.conf
