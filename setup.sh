1873c4cd2dad10759c86d1196d29c36a603378ec
Nick Batsaras <batsarasnikos@gmail.com>
2017-11-26 00:12:51 +0200

    Unstable: Add fontawesome file

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# TODO: Backup first !
+exit 1
+
 ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/vim/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
@@ -9,3 +12,5 @@ ln -s ~/.dotfiles/i3/i3blocks.conf ~/.config/i3/i3blocks.conf
 
 ln -s ~/.dotfiles/mpd/mpd.conf ~/.config/mpd/mpd.conf
 ln -s ~/.dotfiles/ncmpcpp/bindings ~/.ncmpcpp/bindings
+
+cp ~/.dotfiles/fonts/* ~/.fonts/
