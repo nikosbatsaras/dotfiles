715b337554d46d887076ecf4af98f5e65fe16520
Nick Batsaras <batsarasnikos@gmail.com>
2018-06-08 16:25:18 +0300

    Merge watch & download scripts to media.sh

--- bin/watch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo -n "$1" > ~/.media
-
-case "$2" in
-	image)        echo ' image'   >> ~/.media; feh -g 640x480 "$1";;
-	gif | video)  echo ' video'   >> ~/.media; mpv "$1" --loop=inf;;
-	youtube)      echo ' youtube' >> ~/.media; mpv --ytdl-format=best "$1";;
-esac
