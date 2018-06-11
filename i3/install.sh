deb01b5a3a22665e8149dd45c0f5f7c8fff3d420
Nick Batsaras <batsarasnikos@gmail.com>
2018-06-11 21:38:48 +0300

    New installations: 	1) arc-gtk-theme 	2) evince 	3) pcmanfm 	4) pass 	5) redshift

--- i3/install.sh
+++ i3/install.sh
@@ -11,11 +11,24 @@ install alsa-utils
 install imagemagick
 install ttf-inconsolata
 
+install arc-gtk-theme
+install arc-icon-theme
+
+install pcmanfm
+install evince
+install redshift
+
+install pass
+
 backup ~/.config/i3
+backup ~/.gtkrc-2.0
 
 mkdir -p ~/.config/i3
 
 if [ ! -d ~/bin ]; then mkdir ~/bin; fi
 
-ln -s ~/.dotfiles/i3/config  ~/.config/i3/config
-ln -s ~/.dotfiles/i3/lock.sh ~/bin/lock.sh
+ln -s ~/.dotfiles/i3/config         ~/.config/i3/config
+ln -s ~/.dotfiles/i3/lock.sh        ~/bin/lock.sh
+
+ln -s ~/.dotfiles/i3/.gtkrc-2.0     ~/.gtkrc-2.0
+ln -s ~/.dotfiles/i3/redshift.conf  ~/.config/redshift.conf
