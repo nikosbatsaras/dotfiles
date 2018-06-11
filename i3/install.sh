b6f15062782ae851063e64e7816990712b6bf9a3
Nick Batsaras <batsarasnikos@gmail.com>
2018-06-11 23:05:32 +0300

    Add gtk-3.0 config

--- i3/install.sh
+++ i3/install.sh
@@ -22,8 +22,10 @@ install pass
 
 backup ~/.config/i3
 backup ~/.gtkrc-2.0
+backup ~/.config/gtk-3.0
 
 mkdir -p ~/.config/i3
+mkdir    ~/.config/gtk-3.0
 
 if [ ! -d ~/bin ]; then mkdir ~/bin; fi
 
@@ -31,4 +33,5 @@ ln -s ~/.dotfiles/i3/config         ~/.config/i3/config
 ln -s ~/.dotfiles/i3/lock.sh        ~/bin/lock.sh
 
 ln -s ~/.dotfiles/i3/.gtkrc-2.0     ~/.gtkrc-2.0
+ln -s ~/.dotfiles/i3/settings.ini   ~/.config/gtk-3.0/settings.ini
 ln -s ~/.dotfiles/i3/redshift.conf  ~/.config/redshift.conf
