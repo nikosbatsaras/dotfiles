42ad0d8067f36f48a9e83bf64e8c60f418143f01
Nick Batsaras <batsarasnikos@gmail.com>
2018-06-16 22:58:05 +0300

    First commit with i3blocks

--- i3/install.sh
+++ i3/install.sh
@@ -22,6 +22,7 @@ install pass
 install syncthing
 
 backup ~/.config/i3
+backup ~/.i3blocks.conf
 backup ~/.gtkrc-2.0
 backup ~/.config/gtk-3.0
 
@@ -30,9 +31,10 @@ mkdir    ~/.config/gtk-3.0
 
 if [ ! -d ~/bin ]; then mkdir ~/bin; fi
 
-ln -s ~/.dotfiles/i3/config         ~/.config/i3/config
-ln -s ~/.dotfiles/i3/lock.sh        ~/bin/lock.sh
+ln -s ~/.dotfiles/i3/config           ~/.config/i3/config
+ln -s ~/.dotfiles/i3/lock.sh          ~/bin/lock.sh
+ln -s ~/.dotfiles/i3/.i3blocks.conf   ~/.i3blocks.conf
 
-ln -s ~/.dotfiles/i3/.gtkrc-2.0     ~/.gtkrc-2.0
-ln -s ~/.dotfiles/i3/settings.ini   ~/.config/gtk-3.0/settings.ini
-ln -s ~/.dotfiles/i3/redshift.conf  ~/.config/redshift.conf
+ln -s ~/.dotfiles/i3/.gtkrc-2.0       ~/.gtkrc-2.0
+ln -s ~/.dotfiles/i3/settings.ini     ~/.config/gtk-3.0/settings.ini
+ln -s ~/.dotfiles/i3/redshift.conf    ~/.config/redshift.conf
