709971eb4e44504f3da37142f4a29d0e572b52c3
Nikos Batsaras <batsarasnikos@gmail.com>
2019-02-18 20:18:08 +0200

    Remove arc theme

--- i3/install.sh
+++ i3/install.sh
@@ -12,9 +12,6 @@ install imagemagick
 install ttf-inconsolata
 install notification-daemon
 
-install arc-gtk-theme
-install arc-icon-theme
-
 install evince
 install nautilus
 install redshift
@@ -24,11 +21,8 @@ install pass
 
 backup ~/.config/i3
 backup ~/.i3blocks.conf
-backup ~/.gtkrc-2.0
-backup ~/.config/gtk-3.0
 
 mkdir -p ~/.config/i3
-mkdir    ~/.config/gtk-3.0
 
 if [ ! -d ~/bin ]; then mkdir ~/bin; fi
 
@@ -36,8 +30,6 @@ ln -s ~/.dotfiles/i3/config           ~/.config/i3/config
 ln -s ~/.dotfiles/i3/lock.sh          ~/bin/lock.sh
 ln -s ~/.dotfiles/i3/.i3blocks.conf   ~/.i3blocks.conf
 
-ln -s ~/.dotfiles/i3/.gtkrc-2.0       ~/.gtkrc-2.0
-ln -s ~/.dotfiles/i3/settings.ini     ~/.config/gtk-3.0/settings.ini
 ln -s ~/.dotfiles/i3/redshift.conf    ~/.config/redshift.conf
 
 mkdir -p ~/.local/share/fonts
